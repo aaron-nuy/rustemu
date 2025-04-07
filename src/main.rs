879c79c2d8d96cd5ddcde5fe7b9c370f5a18205d
Menai Haroun <aaronjobac@gmail.com>
2025-04-07 21:15:31 +0100

    Pass rom file as parameter

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::env;
 use console::gameboy::{Gameboy};
 
 mod console;
 
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
     let mut gameboy = Gameboy::new();
 
-    gameboy.load("10-bit ops.gb");
+    gameboy.load(&args[1]);
 
     gameboy.run();
 }
