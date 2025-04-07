eef546b4cb51449c208a4d9bc1c703eaf15d6cd5
Menai Haroun <aaronjobac@gmail.com>
2025-04-07 21:19:56 +0100

    Error checking for commandline argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process::exit;
 use console::gameboy::{Gameboy};
 
 mod console;
@@ -7,9 +8,16 @@ mod console;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 2 {
+        println!("Usage: {} [rom file]", args[0]);
+        exit(2);
+    }
+
+    let rom_file = &args[1];
+
     let mut gameboy = Gameboy::new();
 
-    gameboy.load(&args[1]);
+    gameboy.load(rom_file);
 
     gameboy.run();
 }
