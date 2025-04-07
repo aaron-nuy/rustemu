2448cf827105c46c20ef1de8c7bb5785cd1877dd
Menai Haroun <aaronjobac@gmail.com>
2025-04-07 21:10:27 +0100

    Force Cpu reads/writes to memory to pass through mapping function

--- src/console/bus.rs
+++ src/console/bus.rs
@@ -3,22 +3,46 @@ pub struct Bus {
 }
 
 impl Bus {
+    const SB_ADR: u16 = 0xFF01;
+    const SC_ADR: u16 = 0xFF02;
+
+    fn write_to_bus(&mut self, address: u16, value: u8) {
+
+        match address {
+            0xFF02 => {
+                self.ram[Bus::SC_ADR] = value;
+
+                if (value & 0x80) != 0 {
+                    print!("{}", self.ram[Bus::SB_ADR] as char);
+                    self.ram[Bus::SC_ADR] &= 0x7F;
+                }
+            }
+            _ => {
+                self.ram[address as usize] = value;
+            }
+        }
+    }
+
+    fn read_from_bus(&self, address: u16) -> u8 {
+        self.ram[address as usize]
+    }
+
     pub fn write_to_8b(&mut self, address: u16, value: u8) {
-        self.ram[address as usize] = value;
+        self.write_to_bus(address, value);
     }
 
     pub fn read_from_8b(&self, address: u16) -> u8 {
-        self.ram[address as usize]
+        self.read_from_bus(address)
     }
 
     pub fn write_to_16b(&mut self, address: u16, value: u16) {
         let bytes = value.to_le_bytes();
-        self.ram[address as usize] = bytes[0];
-        self.ram[address.wrapping_add(1) as usize] = bytes[1];
+        self.write_to_bus(address, bytes[0]);
+        self.write_to_bus(address.wrapping_add(1), bytes[1]);
     }
 
     pub fn read_from_16b(&self, address: u16) -> u16 {
-        let bytes = [self.ram[address as usize], self.ram[address.wrapping_add(1) as usize]];
+        let bytes = [self.read_from_bus(address), self.read_from_bus(address.wrapping_add(1))];
         u16::from_le_bytes(bytes)
     }
 
